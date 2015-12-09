29ecdd373fc435bb7cc367206f760ee4fd77e753
juvatsana <julienvorabout@hotmail.fr>
2015-12-09 17:24:56 +0100

    Add Converter Service

--- ConverterService/src/Converter.java
+++ ConverterService/src/Converter.java
@@ -5,7 +5,6 @@ import java.rmi.RemoteException;
 import org.tempuri.ConverterSoapProxy;
 
 public class Converter {
-	
 	public double convert(double value, String currencyFrom,String currencyTo) throws RemoteException
 	{
 		return new ConverterSoapProxy().getConversionAmount(currencyFrom, currencyTo, new ConverterSoapProxy().getLastUpdateDate(), new BigDecimal(value,MathContext.DECIMAL64)).doubleValue();
